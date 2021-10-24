87a0fd96e0ac82560967c1df186d90548d0ec14d
nikitaojamae <niojam@ttu.ee>
2021-10-24 16:00:43 +0300

    FR2 create quiz 3 cycle - add testSaveQuizWithQuestions to check if the questions are saved - test failed

--- backoffice/src/test/java/ee/taltech/backoffice/game/service/QuizServiceTest.java
+++ backoffice/src/test/java/ee/taltech/backoffice/game/service/QuizServiceTest.java
@@ -1,6 +1,7 @@
 package ee.taltech.backoffice.game.service;
 
 import ee.taltech.backoffice.game.model.Quiz;
+import ee.taltech.backoffice.game.model.dto.QuestionDto;
 import ee.taltech.backoffice.game.model.dto.QuizDto;
 import ee.taltech.backoffice.game.repository.QuizRepository;
 import ee.taltech.backoffice.game.repository.RoomRepository;
@@ -10,6 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -39,6 +44,21 @@ public class QuizServiceTest {
         when(quizRepository.save(any(Quiz.class))).thenReturn(new Quiz(testQuizDto));
         QuizDto createdQuiz = quizService.createQuiz(testQuizDto, -1L);
         assertThat(createdQuiz.getName()).isEqualTo(testQuizDto.getName());
+    }
+
+    @Test
+    public void testSaveQuizWithQuestions() {
+        List<QuestionDto> questionDtos = new ArrayList<>(Collections.singletonList(
+                new QuestionDto()
+                        .setTitle("first")
+                        .setText("question text")
+                        .setNextQuestionId(null)
+        ));
+        QuizDto testQuizDto = new QuizDto().setName("megaquiz").setAuthorId(-1L).setQuestions(questionDtos);
+        when(quizRepository.save(any(Quiz.class))).thenReturn(new Quiz(testQuizDto));
+        QuizDto createdQuiz = quizService.createQuiz(testQuizDto, -1L);
+        assertThat(createdQuiz.getName()).isEqualTo(testQuizDto.getName());
+        assertThat(createdQuiz.getQuestions().size()).isEqualTo(1);
     }
 
 
